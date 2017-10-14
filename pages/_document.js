df51c722c703d70a76f434a3055a477be3a82fa2
Franco Rabaglia <francoo.hd@gmail.com>
2017-10-14 17:22:50 -0300

    build.

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
     var goodbye = ""
 
     switch (pathname) {
-      case "/index":
+      case "/":
         home = "active"
         break;
 
