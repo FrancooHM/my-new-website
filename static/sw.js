205946bbf4615d4015b305b91b2043cace8b5903
Franco Rabaglia <francoo.hd@gmail.com>
2017-11-13 03:05:00 -0300

    Adding old cache removal from UI.

--- static/sw.js
+++ static/sw.js
@@ -1,3 +1,21 @@
+this.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          if (cacheName === 'v1') return true
+          else return false
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
 this.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v2').then(function(cache) {
