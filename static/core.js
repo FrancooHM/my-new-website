c9857fd435be565993b95313554a8fed4ceb8da2
Franco Rabaglia <francoo.hd@gmail.com>
2017-11-13 03:37:39 -0300

    Build .

--- static/core.js
+++ static/core.js
@@ -1,7 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/static/sw.js', {
-      scope: '/static/'
-    })
+  navigator.serviceWorker.register('/static/sw.js')
     .then(function(reg) {
       // registration worked
       console.log('Registration succeeded. Scope is ' + reg.scope);
