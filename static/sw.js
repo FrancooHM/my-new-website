7e0df2e11dafae805200d011bbd8bab1ddc0df88
Franco Rabaglia <francoo.hd@gmail.com>
2017-11-13 03:53:22 -0300

    Build .

--- static/sw.js
+++ static/sw.js
@@ -1,9 +1,22 @@
+var urlsToCache = [
+  '/',
+  '/static/styles/index.css',
+  '/static/styles/normalize.css',
+  '/static/styles/master.css',
+  '/static/styles/goodbye-master.css',
+  '/static/styles/goodbye.css',
+  '/static/core.js',
+  '/static/favicon.ico',
+]
+
+var FRESH_CACHE_NAME = 'v3'
+
 this.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.filter(function(cacheName) {
-          if (cacheName !== 'v3') return false
+          if (cacheName !== FRESH_CACHE_NAME) return false
           else return true
         }).map(function(cacheName) {
           return caches.delete(cacheName)
@@ -15,17 +28,8 @@ this.addEventListener('activate', function(event) {
 
 this.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v3').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/static/styles/index.css',
-        '/static/styles/normalize.css',
-        '/static/styles/master.css',
-        '/static/styles/goodbye-master.css',
-        '/static/styles/goodbye.css',
-        '/static/core.js',
-        '/static/favicon.ico',
-      ])
+    caches.open(FRESH_CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache)
     })
   )
 })
@@ -34,7 +38,7 @@ this.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(resp) {
       return resp || fetch(event.request).then(function(response) {
-        return caches.open('v3').then(function(cache) {
+        return caches.open(FRESH_CACHE_NAME).then(function(cache) {
           cache.put(event.request, response.clone())
           return response
         })
